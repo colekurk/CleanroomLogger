cf793ecb35fca79fb60cd60ba0817a9ea02bc775
Cole Kurkowski <crk@fastmail.com>
2019-05-22 18:23:40 -0500

    Update Package.swift to work with Swift 5 PM
    The Package.swift file was not functioning correctly with the Swift 5 Package Manager and Xcode 10. This bare bones package should resolve the issues without meaningful changes to the products

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-	name: "CleanroomLogger"
+	name: "CleanroomLogger",
+	products: [
+		.library(name: "CleanroomLogger", targets: ["CleanroomLogger"]),
+	],
+	targets: [
+		.target(name: "CleanroomLogger", path: "Sources"),
+	]
 )
